83e5da3f5016921d6f2feeaf21539d5f54b9a85a
vikrammodh0111 <vikrammodh0111@gmail.com>
2020-11-28 19:08:22 +0530

    Project setup

---
+++ index.js
@@ -0,0 +1,22 @@
+var addition = (a, b) => {
+    return a + b;
+}
+
+var subtract = (a, b) => {
+    return a - b;
+}
+
+var multiply = (a, b) => {
+    return a * b;
+}
+
+var divide = (a, b) => {
+    return a / b;
+}
+
+module.exports = {
+    addition,
+    subtract,
+    multiply,
+    divide,
+}
